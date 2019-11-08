b8fd69324fdf77d14f9caba358c292ec92f8f380
Luiz T. Honda <idzero@gmail.com>
2019-11-08 15:14:23 -0300

    Gotic unit tests implementation

--- src/github.com/concretesolutions/gotic/gotic_test.go
+++ src/github.com/concretesolutions/gotic/gotic_test.go
@@ -1,15 +1,11 @@
 package gotic
 
 import (
+	"errors"
+	"io"
 	"log"
 	"testing"
-
-	"github.com/stretchr/testify/mock"
 )
-
-type MockedExecBashPipedCommand struct {
-	mock.Mock
-}
 
 func TestExecBashPipedCommand(t *testing.T) {
 	logFatal = log.Print
@@ -24,8 +20,31 @@ func TestExecBashPipedCommand(t *testing.T) {
 	}
 }
 
+func buildCommandMockFail(cc string) ([]byte, error) {
+	return []byte("Arthur\n"), errors.New("error")
+}
+
+func buildCommandMock(cc string) ([]byte, error) {
+	return []byte("Arthur\n"), nil
+}
+
+func TestExecBashPipedCommandLogFatal(t *testing.T) {
+	BuildCommand = buildCommandMockFail
+	logFatal = log.Print
+	output, err := ExecBashPipedCommand("echo Arthur\n", true)
+
+	if err == nil {
+		t.Errorf("got '%v'", output)
+	}
+
+	if output != "Arthur" {
+		t.Errorf("got '%v'", output)
+	}
+}
+
 func TestExecBashPipedCommandIgnoreExitCode(t *testing.T) {
-	output, err := ExecBashPipedCommandIgnoreExitCode("echo Arthur", true)
+	BuildCommand = buildCommandMock
+	output, err := ExecBashPipedCommandIgnoreExitCode("echo Arthur\n", true)
 
 	if err != nil {
 		t.Errorf("got '%v'", output)
@@ -36,6 +55,55 @@ func TestExecBashPipedCommandIgnoreExitCode(t *testing.T) {
 	}
 }
 
+func TestExecBashPipedCommandIgnoreExitCodeLogPrintf(t *testing.T) {
+	BuildCommand = buildCommandMockFail
+	output, err := ExecBashPipedCommandIgnoreExitCode("echo Arthur\n", true)
+
+	if err == nil {
+		t.Errorf("got '%v'", output)
+	}
+
+	if output != "Arthur" {
+		t.Errorf("got '%v'", output)
+	}
+}
+
+func TestExecShellScript(t *testing.T) {
+	BuildCommand = buildCommandMockFail
+	out, err := ExecShellScript("echo Arthur", true)
+
+	if err == "" {
+		t.Errorf("got '%v'", out)
+	}
+
+	if out != "Arthur\n" {
+		t.Errorf("got '%v'", out)
+	}
+
+}
+
+func readPromptMock(stdin io.Reader) string {
+	return "y"
+}
+
+func readPromptMockFail(stdin io.Reader) string {
+	return "n"
+}
+
 func TestPrompt(t *testing.T) {
-	Prompt("question", "ls", false)
+	ReadPrompt = readPromptMock
+	result := Prompt("question", "ls", false)
+
+	if result == false {
+		t.Errorf("got '%v'", result)
+	}
+}
+
+func TestPromptFail(t *testing.T) {
+	ReadPrompt = readPromptMockFail
+	result := Prompt("question", "ls", false)
+
+	if result == true {
+		t.Errorf("got '%v'", result)
+	}
 }
